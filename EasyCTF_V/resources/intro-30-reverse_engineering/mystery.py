ba1e49017f8a3010c849238db898fccb6dfc9395
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2018-02-21 17:48:08 +0000

    Test commit

--- EasyCTF_V/resources/intro-30-reverse_engineering/mystery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import binascii
-key = "graAhogG"
-def mystery(s):
-    r = ""
-    for i, c in enumerate(s):
-        r += chr(ord(c) ^ ((i * ord(key[i % len(key)])) % 256))
-    return binascii.hexlify(bytes(r, "utf-8"))
--- EasyCTF_V/resources/reverse_engineering-70-liar/getflag.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int n;
-    scanf("%d", n);
-    // hey that wasn't so hard!
-    if (n == 1337)
-        printf("the flag is easyctf{that's_way_too_easy}");
-}
